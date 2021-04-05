88eee67c41155b7c7ba74a07515a26910f056c18
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2021-04-05 14:41:14 +0530

    using breaking-bad api

---
+++ src/components/ui/Search.js
@@ -0,0 +1,16 @@
+const Search = () => {
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          className="from-control"
+          placeholder="Search characters"
+          autoFocus
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Search;
